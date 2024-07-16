8c640974fb78c2f0104a0636357e90384a4e0b48
DESKTOP-0LVN62V\Modern <askari.zahra94@gmail.com>
2024-07-16 08:33:26 -0500

    create authentication logic

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcrypt from"bcrypt";
+import JWT from "jsonwebtoken"
 import prisma from "../lib/prisma.js";
 
 export const register = async (req,res) =>{
@@ -22,10 +23,43 @@ export const register = async (req,res) =>{
     }
 }
 
-export const login = (req,res) =>{
+export const login = async (req,res) =>{
+
+    const {username, password} = req.body;
+
+    try {
+
+        const user = await prisma.user.findUnique({
+            where :{username}
+        })
+
+        if(!user) return res.status(401).json({message: "Invalid credintial!"})
+
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+
+        if(!isPasswordValid) return res.status(401).json({message: "Invalid credintial!"})
+
+        const age = 1000 * 60 * 60 * 24 * 7 ;
+        const token = JWT.sign({
+            id:user.id
+        }, process.env.JWT_SECRET_KEY, {expiresIn:age})
+
+        
+        res.cookie("toekn", token, {
+            httpOnly:true,
+            secure:true,
+            maxAge: age
+        }).status(200).json({message:"login successfuly"})
+
+    } catch(err){
+        console.log(err);
+        res.status(500).json({message:"Failed to login"})
+    }
     
 }
 
 export const logout = (req,res) =>{
+
+    res.clearCookie("token").status(200).json({message:"logout successful"})
     
 }
--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
 import postRouter from "./routes/post.route.js";
 import authRouter from "./routes/auth.route.js";
 
-
+dotenv.config();
 const app =  express();
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser())
 
 app.use('/api/posts', postRouter);
 app.use('/api/auth', authRouter);
